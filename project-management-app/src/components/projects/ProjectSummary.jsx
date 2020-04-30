ad0463093292b21e0c5b6f79e9a21abe787998d7
Jak Ashuraliev <ashuraliev@gmail.com>
2020-04-29 23:49:37 -0700

    Install redux and setup Reducers

--- project-management-app/src/components/projects/ProjectSummary.jsx
+++ project-management-app/src/components/projects/ProjectSummary.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({ project }) => {
   return (
     <div className="card z-depth-1 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{project.title}</span>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         <br />
         <p>Posted by Jak Ashuraliev</p>
---
+++ project-management-app/src/store/reducers/projectReducer.jsx
@@ -0,0 +1,29 @@
+const initState = {
+  projects: [
+    {
+      id: "1",
+      title: "First Title",
+      content: "Some content goes here.",
+    },
+    {
+      id: "2",
+      title: "Second Title",
+      content: "Some content goes here.",
+    },
+    {
+      id: "3",
+      title: "Third Title",
+      content: "Some content goes here.",
+    },
+  ],
+};
+
+const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log("Created project", action.project);
+  }
+  return state;
+};
+
+export default projectReducer;
